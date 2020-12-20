52547773c9ab88446b21d093a588967e0f21ce32
nightfall-w <rimuli_w@163.com>
2020-12-20 17:45:45 +0800

    支持通过项目名进行搜索

--- project/views.py
+++ project/views.py
@@ -1,19 +1,31 @@
+import coreapi
+import coreschema
 from rest_framework import viewsets, pagination, status, permissions
 from rest_framework.response import Response
+from rest_framework.schemas import AutoSchema
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from .serializers import ProjectSerializer
 from .models import ProjectModel
+from .serializers import ProjectSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    Schema = AutoSchema(manual_fields=[
+        coreapi.Field(name="projectName", required=False, location="query",
+                      schema=coreschema.String(description='项目名'), )
+    ])
+    schema = Schema
     authentication_classes = (JSONWebTokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ProjectSerializer
     pagination_class = pagination.LimitOffsetPagination
 
     def get_queryset(self):
-        return ProjectModel.objects.all().order_by("-id")
+        project_name = self.request.GET.get('projectName')
+        if project_name:
+            return ProjectModel.objects.filter(name__icontains=project_name).order_by("-id")
+        else:
+            return ProjectModel.objects.all().order_by("-id")
 
     def list(self, request, *args, **kwargs):
         projects = self.get_queryset()
